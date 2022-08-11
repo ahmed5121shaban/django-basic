13366dc14c481d7ea1babad8adf98e9b1a91494f
ahmed5121shaban <ahmed.sh5121999@gmail.com>
2022-08-11 17:33:24 +0200

    add files

--- course/admin.py
+++ course/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import course
+
 # Register your models here.
+
+admin.site.register(course)
--- course/views.py
+++ course/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from .models import course
 # Create your views here.
+
+def course(request):
+    context = {'ahmed':'welcome'}
+    return render(request, 'course/course.html', context)
